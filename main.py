c01da93ee8a1ad10966b2e87c66219a10645a248
Ian <iangongwerpersonal@gmail.com>
2021-03-13 11:56:52 -0500

    added top games function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from twitch import *
 
 base_url = 'https://api.twitch.tv/helix/'
@@ -7,4 +8,11 @@ client_secret = 'ody9piwepcovwewqtu17f7gz5m72tq'
 access_token = 't75fol0rpy5xy2b2aciekleeufgosa'
 
 client = TwitchHelix(client_id=client_id, oauth_token=access_token)
-print(client.get_streams())
+
+def get_top_games():
+    data = client.get_top_games()
+    count = 0
+    for dict in data:
+        if count < 11:
+            print(dict['name'])
+            count+=1
