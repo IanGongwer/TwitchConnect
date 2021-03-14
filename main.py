96ffbcfedfaf4e01610147e919eb93ca2ea582da
Ian <iangongwerpersonal@gmail.com>
2021-03-14 12:39:31 -0400

    added more functionality and identification

--- main.py
+++ main.py
@@ -9,10 +9,46 @@ access_token = 't75fol0rpy5xy2b2aciekleeufgosa'
 
 client = TwitchHelix(client_id=client_id, oauth_token=access_token)
 
-def get_top_games():
+def get_top_games(numofgames):
     data = client.get_top_games()
     count = 0
     for dict in data:
-        if count < 11:
-            print(dict['name'])
+        if count <= numofgames:
+            print('Game:', dict['name'], '| Game Id:', dict['id'])
+            count+=1
+
+def get_clips(streamer, numofclips):
+    clips = client.get_clips(broadcaster_id=get_id(streamer))
+    count = 0
+    for clip in clips:
+        if count <= numofclips:
+            print(clip['url'])
+            count+=1
+
+def get_id(streamer):
+    userinfo = client.get_users(streamer)
+    data = userinfo[0]
+    id = data['id']
+    return id
+
+def get_game_id(game):
+    gameinfo = client.get_games(names=game)
+    data = gameinfo[0]
+    game_id = data['id']
+    return game_id
+
+def get_videos(streamer, numofvideos):
+    videos = client.get_videos(user_id=get_id(streamer))
+    count = 0
+    for video in videos:
+        if count <= numofvideos:
+            print(video['title'] + ':', video['url'])
+            count += 1
+
+def get_live_streams(game, numofstreams):
+    livestreams = client.get_streams(game_ids=get_game_id(game))
+    count = 0
+    for stream in livestreams:
+        if count <= numofstreams:
+            print(stream['title'] + ':', stream['user_name'], 'is streaming to', stream['viewer_count'], 'people')
             count+=1
