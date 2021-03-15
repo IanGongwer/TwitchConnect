cfbf0de24f064bf55270960569319e603b96311d
Ian <iangongwerpersonal@gmail.com>
2021-03-14 23:08:58 -0400

    finished the basic functionalities

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import json
 from twitch import *
 
-base_url = 'https://api.twitch.tv/helix/'
-
 client_id = '0u0hkgwkig2j0pvm4s3dl7wpbdgypk'
-client_secret = 'ody9piwepcovwewqtu17f7gz5m72tq'
 access_token = 't75fol0rpy5xy2b2aciekleeufgosa'
+
+client_secret = 'ody9piwepcovwewqtu17f7gz5m72tq'
 
 client = TwitchHelix(client_id=client_id, oauth_token=access_token)
 
@@ -13,15 +12,15 @@ def get_top_games(numofgames):
     data = client.get_top_games()
     count = 0
     for dict in data:
-        if count <= numofgames:
-            print('Game:', dict['name'], '| Game Id:', dict['id'])
+        if count < numofgames:
+            print('Game:', dict['name'].capitalize(), '| Game Id:', dict['id'])
             count+=1
 
 def get_clips(streamer, numofclips):
     clips = client.get_clips(broadcaster_id=get_id(streamer))
     count = 0
     for clip in clips:
-        if count <= numofclips:
+        if count < numofclips:
             print(clip['url'])
             count+=1
 
@@ -41,14 +40,23 @@ def get_videos(streamer, numofvideos):
     videos = client.get_videos(user_id=get_id(streamer))
     count = 0
     for video in videos:
-        if count <= numofvideos:
-            print(video['title'] + ':', video['url'])
+        if count < numofvideos:
+            print(video['title'].capitalize() + ':', video['url'])
             count += 1
 
 def get_live_streams(game, numofstreams):
     livestreams = client.get_streams(game_ids=get_game_id(game))
     count = 0
     for stream in livestreams:
-        if count <= numofstreams:
-            print(stream['title'] + ':', stream['user_name'], 'is streaming to', stream['viewer_count'], 'people')
+        if count < numofstreams:
+            print(stream['title'].capitalize() + ':', stream['user_name'].capitalize(), 'is streaming to', stream['viewer_count'], 'people')
             count+=1
+
+def get_follow_relationship(streamer1, streamer2):
+    info = client.get_user_follows(from_id=get_id(streamer1), to_id=get_id(streamer2))
+    if len(info) != 0:
+        print(streamer1.capitalize(), 'has a follow affiliation with', streamer2.capitalize())
+    else:
+        print(streamer1.capitalize(), 'has no follow affiliation with', streamer2.capitalize())
+
+get_top_games(5)
